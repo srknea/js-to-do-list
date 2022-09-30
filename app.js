cbaff1fa6ffa4da6dd2d80634720121343aab32b
Serkan ISIK <dehnas79@gmail.com>
2022-09-30 16:07:56 +0300

    responsive design

--- app.js
+++ app.js
@@ -184,11 +184,15 @@ function allowDrop(ev) {
 /*end: drag and drop*/
 
 /*start: alert */
-const close = document.getElementsByClassName("closebtn");
-close[0].onclick = function(){
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
     var div = this.parentElement;
     div.style.opacity = "0";
     setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
 }
 /*end: alert */
 
